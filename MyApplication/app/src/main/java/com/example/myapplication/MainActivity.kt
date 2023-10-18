ba5f768ab0c5aac4962e110d6118e4b1e94451ab
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-10-18 13:28:33 +0900

    2023-10-18

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
+import android.widget.RadioButton
 //import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.CheckboxBinding
 
@@ -16,7 +18,20 @@ class MainActivity : AppCompatActivity() {
         //setContentView(binding1.root)
         val binding2 = CheckboxBinding.inflate(layoutInflater)
         setContentView(binding2.root)
+        binding2.visibleCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding2.letsGo.visibility = View.VISIBLE // radioSelect가 binding2에 있는 뷰의 id라고 가정합니다.
+            } else {
+                binding2.letsGo.visibility = View.GONE
+            }
+        }
+        // RadioGroup에 리스너를 설정
+        binding2.radioGroup1.setOnCheckedChangeListener { _, checkedId ->
+            val radio: RadioButton = findViewById(checkedId)
+            binding2.choice.text = radio.text  // choice는 TextView의 ID
+        }
     }
+
     private fun callbackMethod( paramFunc : ()-> Unit){
         Log.d("myTag","MainActivity called this func")
         Handler(Looper.getMainLooper()).postDelayed({
