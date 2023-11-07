aa9b17f0510c7eab6c230228434c17c5ba0e518f
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-07 14:45:27 +0900

    2023-11-07

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,18 +6,24 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
-import android.widget.RadioButton
 //import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.databinding.CheckboxBinding
+//import com.example.myapplication.databinding.TestBinding
+//import com.example.myapplication.databinding.CheckboxBinding
+import com.example.myapplication.databinding.RelativeBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         callbackMethod(paramFunc = {Log.d("myTag", "asd")})
         super.onCreate(savedInstanceState)
+        // * Bindings
         //val binding1 = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(binding1.root)
-        val binding2 = CheckboxBinding.inflate(layoutInflater)
-        setContentView(binding2.root)
+        //val binding2 = CheckboxBinding.inflate(layoutInflater)
+        //val binding3 = Test.inflate(layoutInflater)
+        val binding4 = RelativeBinding.inflate(layoutInflater)
+        setContentView(binding4.root)
+
+
+        /* ! Binding2
         binding2.visibleCheckBox.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 binding2.letsGo.visibility = View.VISIBLE // radioSelect가 binding2에 있는 뷰의 id라고 가정합니다.
@@ -30,6 +36,30 @@ class MainActivity : AppCompatActivity() {
             val radio: RadioButton = findViewById(checkedId)
             binding2.choice.text = radio.text  // choice는 TextView의 ID
         }
+        */
+
+        /* ! Binding4
+        binding4.image22.setOnClickListener{
+            binding4.image22.visibility = View.GONE
+            binding4.btn22.visibility = View.VISIBLE
+        }
+        binding4.btn22.setOnClickListener{
+            binding4.image22.visibility = View.VISIBLE
+            binding4.btn22.visibility = View.GONE
+        }
+        fun allVisible() {
+            binding4.image22.visibility = View.VISIBLE
+            binding4.btn22.visibility = View.VISIBLE
+        }
+        binding4.image22.setOnLongClickListener {
+            allVisible()
+            true // Return true to indicate that the callback consumed the long click
+        }
+        binding4.btn22.setOnLongClickListener {
+            allVisible()
+            true // Return true to indicate that the callback consumed the long click
+        }
+        */
     }
 
     private fun callbackMethod( paramFunc : ()-> Unit){
@@ -39,23 +69,3 @@ class MainActivity : AppCompatActivity() {
         }, 1000L)
     }
 }
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        callbackMethod(paramFunc= {it:String ->
-//            Log.d("myTag", it)
-//        })
-//        callbackMethod { it: String ->
-//            Log.d("myTag", it)
-//        }
-//        callbackMethod(testFun)
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//    private fun callbackMethod( paramFunc : (String)-> Unit){
-//        Log.d("myTag","MainActivity called this func")
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            paramFunc("ㅋㅋㅋㅋㅋ")
-//        }, 1000L)
-//    }
-//    private val testFun: (String) -> Unit = { it ->
-//        Log.d("my tag", it)
-//    }
